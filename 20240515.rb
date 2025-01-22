6963ce9d868d29e2514616a9a06fd292dab6385f
ongaeshi <ongaeshi0621@gmail.com>
2025-01-23 05:26:04 +0900

    feat: Import test https://ksbmyk.github.io/sketch/20240515/

---
+++ 20240515.rb
@@ -0,0 +1,96 @@
+# URL: https://ksbmyk.github.io/sketch/20240515/
+require 'processing'
+using Processing
+
+# RubyKaigi 2024 logo https://rubykaigi.org/2024/ design by mmito1224
+
+setup do
+  createCanvas(400, 400)
+  background("#fced4f")
+  # frameRate(5)
+end
+
+t = 0
+
+draw do
+  t += 1
+  if t % 5 == 0
+    translate(width / 2, height / 2)
+    rotate(HALF_PI - frameCount)
+    draw_frame
+    draw_minsa
+    draw_ruby
+  end
+end
+
+def draw_frame
+  strokeWeight(3)
+  fill("#9cee60")
+  ellipse(0, 0, 280, 280)
+  fill(255)
+  ellipse(0, 0, 250, 250)
+  fill("#9cee60")
+  ellipse(0, 0, 190, 190)
+  fill(255)
+  ellipse(0, 0, 170, 170)
+end
+
+def draw_minsa
+  noStroke
+  fill(0)
+  rect_count = 10 #四角形の数
+
+  3.times do |t|
+    radius = 100 + 10 * t # 円の半径
+    interval_angle = TWO_PI / rect_count # 各四角形の間隔の角度
+
+    # 円周に沿って四角を並べる
+    rect_count.times do |i|
+      angle = i * interval_angle # 四角形の角度
+      x = radius * cos(angle)
+      y = radius * sin(angle)
+      push
+      translate(x, y)
+      rotate(angle) # 四角形を円周に沿って回転
+      rectMode(CENTER)
+
+      if t.even?
+        if i.odd?
+          rect(0, 0, 10, 10)
+        else
+          rect(0, 0 - 10, 10, 10)
+          rect(0, 0 + 10, 10, 10)
+        end
+      else
+        if i.even?
+          rect(0, 0, 10, 10)
+        else
+          rect(0, 0 - 10, 10, 10)
+          rect(0, 0 + 10, 10, 10)
+        end
+      end
+      pop
+    end
+  end
+end
+
+def draw_ruby
+  stroke(0)
+  fill("#ec6158")
+  rotate(TWO_PI + frameCount)
+  beginShape()
+  vertex(-45, 45)
+  vertex(-45, -45)
+  vertex(-10, -70)
+  vertex(65, 0)
+  vertex(-10, 70)
+  endShape(CLOSE)
+
+  line(-45, 45, -10, 25)
+  line(-45, 0, -10, 25)
+  line(-45, 0, -10, -25)
+  line(-45, -45, -10, -25)
+  line(-10, 70, -10, -70)
+  line(-10, 25, 65, 0)
+  line(-10, -25, 65, 0)
+end
